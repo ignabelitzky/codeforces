b87575796746d94c1455c8648cccf5355a88fab5
Ignacio Belitzky <ignabelitzky@gmail.com>
2021-07-28 15:44:23 -0300

    Instructions of the problem added.

--- problem-144A.cpp
+++ problem-144A.cpp
@@ -1,3 +1,36 @@
+/*
+ * 144A. Arrival of the General
+ *
+ * A Ministry for Defense sent a general to inspect the Super Secret Military Squad under the
+ * command of the Colonel SuperDuper. Having learned the news, the colonel ordered to all n
+ * squad soliers to line up on the parade ground.
+ *
+ * By the military charter the soldiers should stand in the order of non-increasing of their height.
+ * But as there's virtually no time to do that, the soldiers lined up in the arbitrary order. However,
+ * the general is rather short-sighted and he thinks that the soldiers lined up correcty if the first
+ * soldier in the line has the maximum height and the last soldier has the minimum height.
+ * Please note that the wat other solders are positioned does not matter, including the case
+ * when there are several soliders whose height is maximum or minimum. Only the heights of the
+ * 'first' and the 'last' soldier are importan.
+ *
+ * For example, the general considers the sequence of heights (4, 3, 4, 2, 1, 1)
+ * correct and the sequence (4, 3, 1, 2, 2) wrong.
+ *
+ * Within one second the colonel can swap any two neighboring soliders. Help him count the
+ * minimum time needed to form a line-up which the general will consider corect.
+ *
+ * Input
+ * The first input line contains the only integer n(2 <= n < = 100) which represents the number of
+ * soldiers in the line contains integers a_1, a_2, ... , a_n (1 <= a_i <= 100) the values
+ * of the soldiers' heights in the order of soldiers' heights' increasing in the order from the
+ * beginning og the line to its end. The numbers are space-separated. Numbers a_1, a_2, ..., a_n are
+ * not necessarily different.
+ *
+ * Output
+ * Print the only integer - the minimum number of second the colonel will need to form a line-
+ * up the general willl like.
+ */
+
 #include <iostream>
 #include <limits>
 
@@ -5,7 +38,8 @@ int main() {
     int n;
     std::cin >> n;
     int maxPos = 0, minPos = 0;
-    int maxVal = std::numeric_limits<int>::min(), minVal = std::numeric_limits<int>::max();
+    int maxVal = std::numeric_limits<int>::min();
+    int minVal = std::numeric_limits<int>::max();
     int value = 0;
     for(int i = 1; i <= n; ++i) {
         std::cin >> value;
